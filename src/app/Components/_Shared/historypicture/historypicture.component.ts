d633341ddba15c40fee027af7bc10048280f541f
tassanai <tassanai.yeeton@gmail.com>
2017-07-17 00:57:03 +0700

    Result ui like a Pinterest

--- src/app/Components/_Shared/historypicture/historypicture.component.ts
+++ src/app/Components/_Shared/historypicture/historypicture.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
 
-@Component({ selector: 'cmp-history-picture', templateUrl: 'historypicture.component.html', styleUrls: ['historypicture.component.css'] })
+@Component({
+    selector: 'cmp-history-picture',
+    templateUrl: 'historypicture.component.html',
+    styleUrls: ['historypicture.component.css']
+})
 
 export class HistoryPictureComponent {
-    @Input() public imageList: string[];
     @Output() public onPickImage = new EventEmitter<any>();
+
+    constructor() {
+
+    }
+
+    public imageList: string[];
+    public imageList1: string[];
+    public imageList2: string[];
+
+    @Input('imageList')
+    set setimageList(value: string[]) {
+        this.imageList = value;
+        this.imageList1 = this.imageList.slice(1, this.imageList.length / 2);
+        this.imageList2 = this.imageList;
+    }
 
     public pickImage(item) {
         this
